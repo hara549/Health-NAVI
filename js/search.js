547adecbe19e32c41f679c64916cc7d6ac5f6688
yhara <y.harata3@gmail.com>
2019-07-01 06:41:07 +0900

    初期コミット

---
+++ js/search.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    'use strict';
+
+    $('.tab li').on('click', function () {
+        var index = $('.tab li').index(this);
+
+        $('.content li').css('display', 'none');
+
+        $('.content li').eq(index).css('display', 'block');
+
+        $('.tab li').removeClass('select');
+
+        $(this).addClass('select');
+    });
+})
